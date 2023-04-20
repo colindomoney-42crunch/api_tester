7891e29cc0884e10a795c3702f820b3cd5eea1a1
Lisa Simpson <lisa@ayepeye.online>
2023-04-20 15:12:51 +0100

    Trying on second branch ...

--- app/api-server.js
+++ app/api-server.js
@@ -416,3 +416,38 @@ api.get('/api/admin/all_users', api_token_check, function (req, res) {
 	})
 });
 
+api.get('/api/admin/ping/:ipAddress', api_token_check, function (req, res) {
+	try {
+        var myIp = new Address4(req.params.ipAddress)
+		var command_to_run = "ping -c 1 " + myIp.address;
+
+		exec(command_to_run, (error, stdout, stderr) => {
+			if (error) {
+				console.log(`error: ${error.message}`);
+				// res.status(400).send('Error:' + error.message)
+				res.status(400).json({ "status": -1, "output": '>>> Failed to ping IP address: ' + error.message });
+				return;
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`);
+				// res.status(400).send('Error:' + stderr.message)
+				res.status(400).json({ "status": -2, "output": '>>> Failed to ping IP address: ' + stderr.message });
+				return;
+			}
+	
+			console.log(`stdout: ${stdout}`);
+	
+			// res.set('Content-Type', 'text/plain');
+			// res.status(200).send(stdout);
+			res.status(200).json({ "status": 0, "output": stdout })
+		});
+
+    }
+    
+    catch (err) {
+        console.log('>>> Failed to convert IP address: ' + err);
+		// res.status(400).send('>>> Failed to convert IP address:' + err)
+		res.status(400).json({ "status": -3, "output": '>>> Failed to ping IP address: ' + err });
+		return;    
+	}
+});
